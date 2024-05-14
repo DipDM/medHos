67d18b8931d0f4628a5d888c011f1b216e5b4cd9
DipDM <161241849+DipDM@users.noreply.github.com>
2024-05-14 11:22:45 +0530

    changes in hero section

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,128 +1,136 @@
-
-
 <template>
-  <!-- Hero section -->
-  <section class="relative bg-logo.jpg bg-center -z-10 bg-no-repeat right-0 left-0">
-            <div class="relative mx-auto max-w-screen-xl px-4 py-10 sm:px-6 lg:flex lg:h-screen lg:items-center lg:px-5">
-                <div class="max-w-xl text-center ltr:sm:text-left rtl:sm:text-right">
-                    <h1 class="text-3xl font-extrabold sm:text-5xl">
-                        Welcome, Where Health is Prioritize
-                        <strong class="block font-extrabold  text-blue-800"> Accensis Hosptials </strong>
-                    </h1>
+    <!-- Hero section -->
+    <section class="relative bg-logo.jpg bg-center overflow-hidden -z-10 bg-no-repeat right-0 left-0 w-screen h-screen">
+        <img src="/image/Re_ Requirement Gathering For Hospital Website/hospitalp5.jpeg" alt="test"
+            class="index-hero-img top-3 right-3 w-32 h-24 lg:w-56 lg:h-44 lg:right-6 lg:top-5 ">
+            <img src="/image/Re_ Requirement Gathering For Hospital Website/hospitalp11.jpeg" alt="test" 
+            class="index-hero-img top-9 left-6 w-40 h-32 lg:hidden">
+            <img src="/image/Re_ Requirement Gathering For Hospital Website/hospitalp11.jpeg" alt="test" 
+            class="index-hero-img hidden lg:block lg:w-56 lg:h-44 lg:top-5 lg:right-80 ">
+            <img src="/image/Re_ Requirement Gathering For Hospital Website/hospitalp1.jpeg" alt="test" 
+            class="index-hero-img bottom-72 left-4 w-40 h-32 lg:hidden">
+            <img src="/image/Re_ Requirement Gathering For Hospital Website/hospitalp1.jpeg" alt="test" 
+            class="index-hero-img  hidden lg:block lg:w-56 lg:h-44 lg:top-56 lg:right-96">
+            <img src="/image/Re_ Requirement Gathering For Hospital Website/hospitalp13.jpeg" alt="test" 
+            class="index-hero-img  w-32 h-24 right-11 top-32 lg:w-56 lg:h-44 lg:right-32 lg:top-56">
+            <img src="/image/Re_ Requirement Gathering For Hospital Website/hospitalp7.jpeg" alt="test" 
+            class="index-hero-img  w-32 h-40 right-6 bottom-60 lg:w-40 lg:h-52 lg:right-80 lg:bottom-24">
+            <img src="/image/Re_ Requirement Gathering For Hospital Website/hospitalp10.jpeg" alt="test" 
+            class="index-hero-img  w-48 h-36 right-32 bottom-20  lg:right-6 lg:w-56 lg:h-44 lg:bottom-24">
+            <img src="/public/android-new-512x512.png" class="absolute scale-150 -z-10 -left-10 top-36 h-32 w-32 md:h-auto md: md:w-auto md:-left-3 md:top-64 ">
+            <img src="/public/android-new-512x512.png" class="absolute -scale-150 -z-10 -right-10 top-96 h-32 w-32 overflow-hidden md:hidden">
+        <div class="absolute mx-auto h-screen w-screen pl-4 pr-0 py-10 items-center justify-center sm:px-6 md:flex md:items-center md:pl-5 md:pr-0 md:-left-64 md:-top-28">
+            <div class="max-w-xl text-center md:justify-start my-52 ltr:sm:text-left rtl:sm:text-right">
+                <h1 class="font-extrabold text-sm md:text-3xl">
+                    Welcome, Where Health is Prioritize
+                    <strong class="block font-extrabold text-sm md:text-6xl text-blue-800"> Accensis Hosptials </strong>
+                </h1>
 
-                    <p class="mt-4 max-w-lg sm:text-xl/relaxed">
-                        Nagpur's Leading Superspeaciality Hosptial & Research Center
-                    </p>
+                <p class="mt-4 max-w-lg sm:text-xl/relaxed text-sm md:text-2xl">
+                    Nagpur's Leading Superspeaciality Hosptial & Research Center
+                </p>
 
-                    <div class="mt-8 flex justify-center flex-wrap gap-4 text-center">
-                        <a href="#"
-                            class="block w-full rounded bg-blue-600 px-12 py-3 text-sm font-medium text-white shadow hover:bg-blue-700 focus:outline-none focus:ring active:bg-blue-500 sm:w-auto">
-                            Get Started
-                        </a>
-                    </div>
-                </div>
-                <div class="flex items-center justify-center gap-8 border rounded-lg m-3 ">
-                    <img src="../resources/image/Re_ Requirement Gathering For Hospital Website/hospitalp1.jpeg" alt="test" class="index-hero-img">
-                    <div class="grid gap-6 ">
-                        <img src="../resources/image/Re_ Requirement Gathering For Hospital Website/hospitalp11.jpeg" alt="test" class="index-hero-img">
-                        <img src="../resources/image/Re_ Requirement Gathering For Hospital Website/hospitalp13.jpeg" alt="test" class="index-hero-img">
-                    </div>
-                    <img src="../resources/image/Re_ Requirement Gathering For Hospital Website/hospitalp19.jpeg" alt="test" class="index-hero-img">
-                </div>
-            </div>
-        </section>
-
-
-        <!-- Companies Feature Line -->
-        <div id="features-line" class="container mt-0 lg:mt-4 ">
-            <div class="border rounded-lg overflow-hidden flex justify-center p-4">
-                <div id="line4" class="flex gap-8 p-6">
-                    <h3 class="my-0 mx-2 whitespace-nowrap font-display text-2xl font-semibold leading-7">Oncology &
-                        Oncosurgery</h3>
-                    <span>•</span>
-                    <h3 class="my-0 mx-2 whitespace-nowrap font-display text-2xl font-semibold leading-7">Gynaecology
-                    </h3>
-                    <span>•</span>
-                    <h3 class="my-0 mx-2 whitespace-nowrap font-display text-2xl font-semibold leading-7">Our
-                        Specialities</h3>
-                    <span>•</span>
-                    <h3 class="my-0 mx-2 whitespace-nowrap font-display text-2xl font-semibold leading-7">Maxillofacial
-                    </h3>
-                    <span>•</span>
-                    <h3 class="my-0 mx-2 whitespace-nowrap font-display text-2xl font-semibold leading-7">Neurosurgery
-                    </h3>
-                    <span>•</span>
-                    <h3 class="my-0 mx-2 whitespace-nowrap font-display text-2xl font-semibold leading-7">Pulmunology
-                    </h3>
-                    <span>•</span>
-                    <h3 class="my-0 mx-2 whitespace-nowrap font-display text-2xl font-semibold leading-7">General
-                        Surgery</h3>
-                    <span>•</span>
-                    <h3 class="my-0 mx-2 whitespace-nowrap font-display text-2xl font-semibold leading-7">Urology &
-                        UroSurgery</h3>
-                    <span>•</span>
-                    <h3 class="my-0 mx-2 whitespace-nowrap font-display text-2xl font-semibold leading-7">Ayurvedic</h3>
-                    <span>•</span>
+                <div class="mt-3 flex justify-center flex-wrap gap-4 text-center">
+                    <button
+                        class="block rounded bg-blue-600 px-6 py-2 text-sm font-medium text-white shadow hover:bg-blue-700 focus:outline-none focus:ring active:bg-blue-500 sm:w-auto">Get
+                        Started</button>
                 </div>
             </div>
         </div>
+    </section>
 
-        <!-- reviews -->
-        <section class=" mx-6 ">
-            <h1 class="text-center text-4xl font-bold">
-                Reviews
-            </h1>
-            <div class="flex items-start justify-center m-4 gap-1">
-                <i class="fa-solid fa-star text-yellow-500"></i>
-                <i class="fa-solid fa-star text-yellow-500"></i>
-                <i class="fa-solid fa-star text-yellow-500"></i>
-                <i class="fa-solid fa-star text-yellow-500"></i>
-                <i class="fa-solid fa-star text-yellow-500"></i>
-                <p class="text-xs font-bold">5.0 out 5.0 Ratings</p>
+
+    <!-- Companies Feature Line -->
+    <div id="features-line" class="container mt-0 lg:mt-4 ">
+        <div class="border rounded-lg overflow-hidden flex justify-center p-4">
+            <div id="line4" class="flex gap-8 p-6">
+                <h3 class="my-0 mx-2 whitespace-nowrap font-display text-2xl font-semibold leading-7">Oncology &
+                    Oncosurgery</h3>
+                <span>•</span>
+                <h3 class="my-0 mx-2 whitespace-nowrap font-display text-2xl font-semibold leading-7">Gynaecology
+                </h3>
+                <span>•</span>
+                <h3 class="my-0 mx-2 whitespace-nowrap font-display text-2xl font-semibold leading-7">Our
+                    Specialities</h3>
+                <span>•</span>
+                <h3 class="my-0 mx-2 whitespace-nowrap font-display text-2xl font-semibold leading-7">Maxillofacial
+                </h3>
+                <span>•</span>
+                <h3 class="my-0 mx-2 whitespace-nowrap font-display text-2xl font-semibold leading-7">Neurosurgery
+                </h3>
+                <span>•</span>
+                <h3 class="my-0 mx-2 whitespace-nowrap font-display text-2xl font-semibold leading-7">Pulmunology
+                </h3>
+                <span>•</span>
+                <h3 class="my-0 mx-2 whitespace-nowrap font-display text-2xl font-semibold leading-7">General
+                    Surgery</h3>
+                <span>•</span>
+                <h3 class="my-0 mx-2 whitespace-nowrap font-display text-2xl font-semibold leading-7">Urology &
+                    UroSurgery</h3>
+                <span>•</span>
+                <h3 class="my-0 mx-2 whitespace-nowrap font-display text-2xl font-semibold leading-7">Ayurvedic</h3>
+                <span>•</span>
             </div>
-            <div class="grid md:flex justify-between gap-3 ">
-                <figure class="max-w-screen  mx-auto text-center my-2 ">
-                    <i class="fa-solid fa-quote-right"></i>
-                    <blockquote>
-                        <p class="text-sm md:text-xl italic font-medium">"It was a very good experience with Dr.Sudhir Sir he
-                            helped my wife with critical circumstances and provide me good faculty and everything to my
-                            wife and now she is well and fit because of his treatment
-                            Thank you so much Dr.Sudhir Sir"</p>
-                    </blockquote>
-                    <figcaption class="flex items-center justify-center mt-6 space-x-3 rtl:space-x-reverse">
-                        <div class="w-6 h-6 rounded-full bg-green-300">
-                            <p>S</p>
-                        </div>
-                        <div class="flex items-center divide-x-2 rtl:divide-x-reverse divide-gray-500">
-                            <cite class="pe-3 font-medium text-gray-900 ">Sanjit Mukherjee</cite>
-                            <cite class="ps-3 text-sm text-gray-500 ">comment from googlemaps</cite>
-                        </div>
-                    </figcaption>
-                </figure>
-                <figure class="max-w-screen mx-auto text-center my-2">
-                    <i class="fa-solid fa-quote-right"></i>
-                    <blockquote>
-                        <p class="text-sm md:text-xl italic font-medium">"My pt got admitted to accencis hospital in a very
-                            critical condition...... We got the best treatment here.... Also the director Dr. Sudhir
-                            Meshram is a very kind and helpful person...... Staff of the hospital is caring... Very much
-                            satisfied with this hospital"</p>
-                    </blockquote>
-                    <figcaption class="flex items-center justify-center mt-6 space-x-3 rtl:space-x-reverse">
-                        <div class="w-6 h-6 rounded-full bg-cyan-300">
-                            <p>D</p>
-                        </div>
-                        <div class="flex items-center divide-x-2 rtl:divide-x-reverse divide-gray-500">
-                            <cite class="pe-3 font-medium text-gray-900 ">Devraj Pandey</cite>
-                            <cite class="ps-3 text-sm text-gray-500 ">comment from googlemaps</cite>
-                        </div>
-                    </figcaption>
-                </figure>
-            </div>
-            <div class="flex justify-center">
-                <button class="border-blue-950 bg-blue-700 p-2 rounded-lg text-white hover:bg-blue-300  ">Read More
-                    Reviews</button>
-            </div>
-        </section>
+        </div>
+    </div>
+
+    <!-- reviews -->
+    <section class=" mx-6 ">
+        <h1 class="text-center text-4xl font-bold">
+            Reviews
+        </h1>
+        <div class="flex items-start justify-center m-4 gap-1">
+            <i class="fa-solid fa-star text-yellow-500"></i>
+            <i class="fa-solid fa-star text-yellow-500"></i>
+            <i class="fa-solid fa-star text-yellow-500"></i>
+            <i class="fa-solid fa-star text-yellow-500"></i>
+            <i class="fa-solid fa-star text-yellow-500"></i>
+            <p class="text-xs font-bold">5.0 out 5.0 Ratings</p>
+        </div>
+        <div class="grid md:flex justify-between gap-3 ">
+            <figure class="max-w-screen  mx-auto text-center my-2 ">
+                <i class="fa-solid fa-quote-right"></i>
+                <blockquote>
+                    <p class="text-sm md:text-xl italic font-medium">"It was a very good experience with Dr.Sudhir Sir
+                        he
+                        helped my wife with critical circumstances and provide me good faculty and everything to my
+                        wife and now she is well and fit because of his treatment
+                        Thank you so much Dr.Sudhir Sir"</p>
+                </blockquote>
+                <figcaption class="flex items-center justify-center mt-6 space-x-3 rtl:space-x-reverse">
+                    <div class="w-6 h-6 rounded-full bg-green-300">
+                        <p>S</p>
+                    </div>
+                    <div class="flex items-center divide-x-2 rtl:divide-x-reverse divide-gray-500">
+                        <cite class="pe-3 font-medium text-gray-900 ">Sanjit Mukherjee</cite>
+                        <cite class="ps-3 text-sm text-gray-500 ">comment from googlemaps</cite>
+                    </div>
+                </figcaption>
+            </figure>
+            <figure class="max-w-screen mx-auto text-center my-2">
+                <i class="fa-solid fa-quote-right"></i>
+                <blockquote>
+                    <p class="text-sm md:text-xl italic font-medium">"My pt got admitted to accencis hospital in a very
+                        critical condition...... We got the best treatment here.... Also the director Dr. Sudhir
+                        Meshram is a very kind and helpful person...... Staff of the hospital is caring... Very much
+                        satisfied with this hospital"</p>
+                </blockquote>
+                <figcaption class="flex items-center justify-center mt-6 space-x-3 rtl:space-x-reverse">
+                    <div class="w-6 h-6 rounded-full bg-cyan-300">
+                        <p>D</p>
+                    </div>
+                    <div class="flex items-center divide-x-2 rtl:divide-x-reverse divide-gray-500">
+                        <cite class="pe-3 font-medium text-gray-900 ">Devraj Pandey</cite>
+                        <cite class="ps-3 text-sm text-gray-500 ">comment from googlemaps</cite>
+                    </div>
+                </figcaption>
+            </figure>
+        </div>
+        <div class="flex justify-center">
+            <button class="border-blue-950 bg-blue-700 p-2 rounded-lg text-white hover:bg-blue-300  ">Read More
+                Reviews</button>
+        </div>
+    </section>
 
 </template>
 
